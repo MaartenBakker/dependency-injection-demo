3b2e8a5551f560dac9add937ba9db2fe9b66570c
Maarten Bakker <maartenbakker@gmail.com>
2020-10-14 20:53:02 +0200

    Refeactor main method

--- src/main/java/com/maartenmusic/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
+++ src/main/java/com/maartenmusic/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
@@ -13,41 +13,12 @@ public class DependencyInjectionDemoApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
-
-		System.out.println("---------- Property");
-
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
-		System.out.println(propertyInjectedController.getGreeting());
-
-		System.out.println("---------- Setter");
-
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("---------- Constructor");
-
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
-		System.out.println(constructorInjectedController.getGreeting());
-
-		System.out.println("---------- MyController");
-
-		System.out.println(myController.getGreeting());
-
-		System.out.println("---------- i18n");
-
-
-		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-
-		System.out.println(i18nController.sayHello());
-
-
-
-
+		System.out.println(myController.sayHello());
+		System.out.println(ctx.getBean(PropertyInjectedController.class).getGreeting());
+		System.out.println(ctx.getBean(SetterInjectedController.class).getGreeting());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).getGreeting());
+		System.out.println(ctx.getBean(I18nController.class).sayHello());
+		
 
 	}
 
