617f1eab111a64726f8c8d2ac3b23a2094080581
Maarten Bakker <maartenbakker@gmail.com>
2020-10-18 14:59:44 +0200

    Environmental variables override property file variables demo

--- src/main/java/com/maartenmusic/dependencyinjectiondemo/config/FakeDataSourceConfig.java
+++ src/main/java/com/maartenmusic/dependencyinjectiondemo/config/FakeDataSourceConfig.java
@@ -1,14 +1,19 @@
 package com.maartenmusic.dependencyinjectiondemo.config;
 
 import com.maartenmusic.dependencyinjectiondemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class FakeDataSourceConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${maartenmusic.username}")
     String user;
@@ -20,7 +25,7 @@ public class FakeDataSourceConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME")); // Env variables override properties file variables
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
